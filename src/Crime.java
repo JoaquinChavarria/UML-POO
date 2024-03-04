2dfd353e515f2e33b21ca221714f1f627e9df7cb
Joaquin <joacochmjkl@gmail.com>
2024-03-04 00:03:00 -0600

    JC: Primeras clases

---
+++ src/Crime.java
@@ -0,0 +1,14 @@
+package src;
+// Clase Crimen: Identifica un crimen en el expediente
+public class Crime {
+    
+    private String name;
+
+    public Crime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,14 @@
+package src;
+// Clase Persona: Identific una persona en el expediente
+public class Person {
+    
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
